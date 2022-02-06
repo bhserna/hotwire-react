0d44f043482bcb4d0925317a55073bf9f1ce8e2d
bhserna <bhserna@gmail.com>
2022-02-06 06:48:02 -0600

    Render react element on stimulus controller

---
+++ app/javascript/controllers/hello_controller.jsx
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+import * as React from 'react'
+import * as ReactDOM from "react-dom";
+import App from "./../app"
+
+export default class extends Controller {
+  connect() {
+    ReactDOM.render(<App />, this.element);
+  }
+}
